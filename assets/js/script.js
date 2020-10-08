b07ef5c9fa0e3524a87fc0fc359e41c3c30eb32d
os-lapierre <os.lapierre@gmail.com>
2020-10-08 05:05:44 -0300

    commint n6: agregando funciones a mi archivo js

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+$('#btn_coff,#btn_dmark,#btn_rico,#btn_cup,#btn_suri,#btn_iguana').click(function (){
+    alert("Lo sentimos, el sitio aun esta en construccion.")
+});
